9c05bb50e5ae24fcf6177a32d5c2b97517765acd
Morin <zachary.morin@mymail.champlain.edu>
2023-09-05 15:43:27 -0400

    new stuff

---
+++ C++Projects/MorinCPPProject/CSI140/1001.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sstream> // Stringstream class
+using namespace std;
+
+int main() {
+
+    int x;
+    cin >> x;
+    int y;
+    cin >> y;
+    int z = x + y;
+
+    stringstream stream; // Create instance of stringstream
+    stream << z; // Put last integer into stringstream instance.
+    string a; // Declare a to be a string for the final integer.
+    stream >> a; // Put stream into the final string.
+
+    cout << "X = " + a + "\n";
+
+    return 0;
+}
